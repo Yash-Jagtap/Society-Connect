01147f2bad84e8218a914bfa09bfd7e6ffe0bcc5
Yash Jagtap <yash.jagtap@spit.ac.in>
2022-07-01 00:03:14 +0530

    added static_path

--- societyconnect/settings.py
+++ societyconnect/settings.py
@@ -140,7 +140,7 @@ LOGIN_URL = '/login/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # CELERY SETTINGS
 
